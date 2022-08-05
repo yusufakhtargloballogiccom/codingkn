20f95c9364199ea90789f862219e0155a143aa0d
yusufakhtargloballogiccom <110605362+yusufakhtargloballogiccom@users.noreply.github.com>
2022-08-05 11:27:42 +0530

    Add files via upload

---
+++ Exh2Demo.java
@@ -0,0 +1,69 @@
+class Exh2Demo
+
+{
+
+ public static void main(String[] args)
+
+ {
+
+ int a=10,b;
+
+ try
+
+ {
+
+ System.out.println("in the try block");
+
+ System.out.println("before parseInt");
+
+ b=Integer.parseInt(args[0]);
+
+ System.out.println("after parseInt");
+
+  int c;
+
+  c=a/b;
+
+System.out.println("c value is "+c);
+
+System.out.println("end of try");
+
+ }
+
+ catch(ArithmeticException ae)
+
+ {
+
+ System.out.println("arithmetic error -catch block");
+
+ }
+
+ catch(ArrayIndexOutOfBoundsException aioe)
+
+ {
+
+  System.out.println(aioe.toString());
+
+ }
+
+ catch(Exception e)
+
+ {
+
+  e.printStackTrace();
+
+ }
+
+ finally
+
+ {
+
+  System.out.println(" finally block");
+
+ }
+
+ System.out.println("end of the prg");
+
+ }
+
+}
